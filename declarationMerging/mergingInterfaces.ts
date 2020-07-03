aa811510f772232c7a8fa8c8ab623998bc96555b
richar <richarfang@xiaoe-tech.com>
2020-07-03 10:30:19 +0800

    update

---
+++ declarationMerging/mergingInterfaces.ts
@@ -0,0 +1,18 @@
+interface Document {
+    createElement(tagName: any): Element;
+}
+interface Document {
+    createElement(tagName: "div"): HTMLDivElement;
+    createElement(tagName: "span"): HTMLSpanElement;
+}
+interface Document {
+    createElement(tagName: string): HTMLElement;
+    createElement(tagName: "canvas"): HTMLCanvasElement;
+}
+
+interface Document {
+    createElement(tagName: any): HTMLFormElement;
+}
+
+let a: Document;
+let b = a.createElement('span')
